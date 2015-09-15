97b4bdf4cbf0576c742ba7a43fcbe8caf65fbe1d
Vilyever <250450035@163.com>
2015-09-15 17:06:35 +0800

    release 1.0.0

---
+++ README.md
@@ -0,0 +1,42 @@
+# AndroidContextHolder
+获取应用context
+
+## Import
+[JitPack](https://jitpack.io/)
+
+Add it in your project's build.gradle at the end of repositories:
+
+```gradle
+repositories {
+  // ...
+  maven { url "https://jitpack.io" }
+}
+```
+
+Step 2. Add the dependency in the form
+
+```gradle
+dependencies {
+  compile 'com.github.vilyever:AndroidContextHolder:1.0.0'
+}
+```
+
+## Usage
+```java
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        VDContextHolder.initial(this); // initial
+    }
+}
+
+VDContextHolder.getContext(); // then call this anywhere
+
+```
+
+## License
+[Apache License Version 2.0](http://www.apache.org/licenses/LICENSE-2.0.txt)
+
---
+++ vdcontextholder/src/main/java/com/vilyever/vdcontextholder/VDContextHolder.java
@@ -0,0 +1,85 @@
+package com.vilyever.vdcontextholder;
+
+import android.app.Application;
+import android.content.Context;
+import android.util.Log;
+
+/**
+ * VDContextHolder
+ * AndroidContextHolder <com.vilyever.vdcontextholder>
+ * Created by vilyever on 2015/9/15.
+ * Feature:
+ */
+public class VDContextHolder {
+    private final VDContextHolder self = this;
+
+    static Context ApplicationContext;
+
+    /* #Constructors */    
+    
+    /* #Overrides */    
+    
+    /* #Accessors */     
+     
+    /* #Delegates */     
+     
+    /* #Private Methods */    
+    
+    /* #Public Methods */
+    public static void initial(Context context) {
+        ApplicationContext = context;
+    }
+
+    public static Context getContext() {
+        if (ApplicationContext == null) {
+            try {
+                Application application = (Application) Class.forName("android.app.ActivityThread")
+                        .getMethod("currentApplication").invoke(null, (Object[]) null);
+                if (application != null) {
+                    Log.d(VDContextHolder.class.getName(), "ContextHolder is not initialed, it is recommend to initial with application context.");
+                    return application;
+                }
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            try {
+                Application application = (Application) Class.forName("android.app.AppGlobals")
+                        .getMethod("getInitialApplication").invoke(null, (Object[]) null);
+                if (application != null) {
+                    Log.d(VDContextHolder.class.getName(), "ContextHolder is not initialed, it is recommend to initial with application context.");
+                    return application;
+                }
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            try {
+                Object activityThread = Class.forName("android.app.ActivityThread")
+                        .getMethod("systemMain").invoke(null, (Object[]) null);
+                Context context = (Context) activityThread.getClass().getMethod("getSystemContext").invoke(activityThread, (Object[]) null);
+                if (context != null) {
+                    Log.d(VDContextHolder.class.getName(), "ContextHolder is not initialed, it is recommend to initial with application context.");
+                    return context;
+                }
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+            throw new IllegalStateException("ContextHolder is not initialed, it is recommend to initial with application context.");
+        }
+        return ApplicationContext;
+    }
+
+    /* #Classes */
+
+    /* #Interfaces */     
+     
+    /* #Annotations @interface */    
+    
+    /* #Enums */
+}
